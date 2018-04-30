72f8db52fa425ed9cacedef6679e5675299b6c4c
Carlos Lázaro Costa <lazaro.costa.carles@gmail.com>
2018-04-30 05:41:11 +0200

    CORS debug

--- index.js
+++ index.js
@@ -282,7 +282,7 @@ function updateChapter(event, title, chapter, maxChapter, score, malId) {
 
     $.support.cors = true;
     $.ajax({
-      url: /*https://cors-anywhere.herokuapp.com/*/`https://myanimelist.net/api/animelist/update/${malId}.xml`,
+      url: `https://cors-anywhere.herokuapp.com/https://myanimelist.net/api/animelist/update/${malId}.xml`,
       cache: false,
       type: 'POST',
       data: `<?xml version="1.0" encoding="UTF-8"?><entry>${toXML(entry)}</entry>`,
