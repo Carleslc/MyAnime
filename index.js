89236d09726ef10cb55630e8d1951aa0a3f6b291
Carlos Lázaro Costa <lazaro.costa.carles@gmail.com>
2018-05-01 22:33:25 +0200

    Check credentials

--- index.js
+++ index.js
@@ -349,7 +349,7 @@ var checkpoint;
 function updatePassword() {
   let password = $("#password").val().trim();
   if (password != '') {
-    GET_CORS(`https://${user.name}:${user.pass}@myanimelist.net/api/account/verify_credentials.xml`, (body, status) => {
+    GET_CORS(`https://${user}:${password}@myanimelist.net/api/account/verify_credentials.xml`, (body, status) => {
       if (status === 200) {
         storage.set('password', password);
         $("#set-password").modal('hide');
