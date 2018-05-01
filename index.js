0658c26bbc9f038315f66d58a36dc12daef4f3b6
Carlos Lázaro Costa <lazaro.costa.carles@gmail.com>
2018-05-01 18:57:08 +0200

    Debug

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ function POST_CORS(url, data, success, error, opts) {
 function auth(user, password) {
   return function(opts) {
     opts.beforeSend = function(xhr) {
+      console.log("Set Authorization");
       xhr.setRequestHeader("Authorization", "Basic " + btoa(user + ":" + password));
     }
   };
